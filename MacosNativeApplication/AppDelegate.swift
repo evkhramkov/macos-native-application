934bf3875d480e69abb514ef08e6b65397b79351
Evgenii Khramkov <evkhramk@microsoft.com>
2018-05-18 12:37:06 +0300

    Initial Commit

---
+++ MacosNativeApplication/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacosNativeApplication
+//
+//  Created by Evgenii Khramkov on 5/18/18.
+//  Copyright © 2018 Evgenii Khramkov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacosNativeApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacosNativeApplication
+//
+//  Created by Evgenii Khramkov on 5/18/18.
+//  Copyright © 2018 Evgenii Khramkov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
